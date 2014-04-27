e818430823bfd41c8f8ce4aeab825073a97065a3
pculka <pculka@users.noreply.github.com>
2014-04-27 22:07:35 +0200

    first parsing and output

--- srt2vtt.php
+++ srt2vtt.php
@@ -4,19 +4,74 @@
  * Date: 27.4.2014
  * Time: 19:14
  */
+$srt2vttConf = array();
 if (file_exists("srt2vtt.conf.php"))
     require_once("srt2vtt.conf.php");
 
 try {
     // access the get, check for valid filename
-    $fn = filter_input(INPUT_GET, "i", FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>'%^[^"<>|:*?/]+\.srt$%m') ));
+    $fn  = filter_input(INPUT_GET, "i", FILTER_VALIDATE_REGEXP, array( "options" => array( "regexp" => '%^[^"<>|:*?/]+\.srt$%m') ) );
+    $fn = 'Game of Thrones - S04E01.CZ.srt';
     if ($fn === false) {
-        throw new Exception(_("Filename invalid"));
+        throw new Exception(_("Specified input is invalid"));
     }
 
     if (file_exists($fn)) {
         // read the file
-        $file = fopen($fn, 'r');
+        $subtitleLines   = file($fn);
+
+        if (empty($subtitleLines)) throw new Exception(_("Subtitle file empty"));
+        // prepare the result
+
+        $result = "WEBVTT\n\n";
+        define('SRT_STATE_SUBNUMBER', 0);
+        define('SRT_STATE_TIME',      1);
+        define('SRT_STATE_TEXT',      2);
+        define('SRT_STATE_BLANK',     3);
+
+        $subs    = array();
+        $state   = SRT_STATE_SUBNUMBER;
+        $subNum  = 0;
+        $subText = '';
+        $subTime = '';
+
+        foreach($subtitleLines as $line) {
+            switch($state) {
+                case SRT_STATE_SUBNUMBER:
+                    $subNum = trim($line);
+                    $state  = SRT_STATE_TIME;
+                    break;
+
+                case SRT_STATE_TIME:
+                    $subTime = trim($line);
+                    $state   = SRT_STATE_TEXT;
+                    break;
+
+                case SRT_STATE_TEXT:
+                    if (trim($line) == '') {
+                        $sub = new stdClass;
+                        $sub->number = $subNum;
+                        list($sub->startTime, $sub->stopTime) = explode(' --> ', $subTime);
+                        $sub->text   = $subText;
+                        $subText     = '';
+                        $state       = SRT_STATE_SUBNUMBER;
+                        $subs[]      = $sub;
+                    } else {
+                        $subText .= trim($line)."\n";
+                    }
+                    break;
+            }
+        }
+
+        foreach ($subs as $sub) {
+            $result .= $sub->number."\n";
+            $result .= $sub->startTime." --> ".$sub->stopTime."\n";
+            $result .= $sub->text."\n";
+        }
+
+
+        header('Content-type: text/plain; charset=utf-8');
+        echo $result;
 
     } else {
         throw new Exception(_("File not found"));
@@ -24,9 +79,11 @@ try {
 
 } catch (Exception $e) {
     var_dump( $e->getMessage() );
-
+    // append the message as a subtitle lasting 5 hours (to notify the user about the error within video)
+    // #todo
 } finally {
     // append information if configured
+    // #todo
 
 
 
